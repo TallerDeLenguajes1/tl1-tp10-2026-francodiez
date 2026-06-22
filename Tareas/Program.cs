2681aab3c11b69ecd2bcb79d15551475d771a659
Franco <francodiezgomez@gmail.com>
2026-06-22 09:28:37 -0300

    Ejercicio 1

---
+++ Tareas/Program.cs
@@ -0,0 +1,35 @@
+﻿using clase;
+using System.Net.Http;
+using System.Text.Json;
+using System;
+using System.IO;
+//string estado;
+
+HttpClient client = new HttpClient();
+
+HttpResponseMessage respuesta = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/");
+respuesta.EnsureSuccessStatusCode();
+
+string contenidoresp= await respuesta.Content.ReadAsStringAsync();
+
+List<Tarea> listaTarea = JsonSerializer.Deserialize<List<Tarea>>(contenidoresp);
+
+foreach (var Tar in listaTarea)
+{
+    if (Tar.completed == false)
+    {
+        Console.WriteLine($"Titulo tarea:{Tar.title} \nEstado:Pendiente");
+    } 
+}
+
+foreach (var Tar in listaTarea)
+{
+    if (Tar.completed == true)
+    {
+        Console.WriteLine($"Titulo tarea:{Tar.title} \nEstado:Completada");
+    } 
+}
+
+string jsonString = JsonSerializer.Serialize(listaTarea);
+
+File.WriteAllText("tareas.json", jsonString);
---
+++ Tareas/clase.cs
@@ -0,0 +1,9 @@
+namespace clase;
+
+public class Tarea
+{
+    public int userId {get; set;}
+    public int id {get;set;}
+    public string title {get;set;}
+    public bool completed {get;set;}
+}
