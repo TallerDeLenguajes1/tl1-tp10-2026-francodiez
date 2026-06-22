c7088c9d005bd647e447096f83d7e7b38644632b
Franco <francodiezgomez@gmail.com>
2026-06-22 10:34:58 -0300

    Ejercicio 2

---
+++ Usuarios/Program.cs
@@ -0,0 +1,27 @@
+﻿using usuario;
+using System.Net.Http;
+using System.Text.Json;
+using System;
+using System.IO;
+using System.Linq; // Necesario para usar .Take() para limitar el recorrido en la lista
+
+HttpClient client = new HttpClient();
+
+HttpResponseMessage respuesta = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+respuesta.EnsureSuccessStatusCode();
+
+string contenidoresp= await respuesta.Content.ReadAsStringAsync();
+
+List<Usuario> listaUsuarios = JsonSerializer.Deserialize<List<Usuario>>(contenidoresp);
+
+foreach (var Usu in listaUsuarios.Take(5))
+{
+    Console.WriteLine($"Nombre:{Usu.name}\nCorreo:{Usu.email}\nDomicilio:");
+    Console.WriteLine($" Calle:{Usu.address.street}\n Suite:{Usu.address.suite}\n Ciudad:{Usu.address.city}\n Codigo postal:{Usu.address.zipcode}");
+    Console.WriteLine($" Ubicacion geografica:\n  Latitud:{Usu.address.geo.lat}\n  Longitud:{Usu.address.geo.lng}\n\n");
+}
+
+
+string jsonString = JsonSerializer.Serialize(listaUsuarios);
+
+File.WriteAllText("usuarios.json", jsonString); 
---
+++ Usuarios/usuario.cs
@@ -0,0 +1,33 @@
+namespace usuario;
+
+public class geo
+{
+    public string lat{get;set;}
+    public string lng{get;set;}
+}
+public class address
+{
+    public string street{get;set;}
+    public string suite{get;set;}
+    public string city{get;set;}
+    public string zipcode{get;set;}
+    public geo geo{get;set;}
+}
+
+public class company
+{
+    public string name{get;set;}
+    public string catchPhrase{get;set;}
+    public string bs{get;set;}
+}
+public class Usuario
+{
+    public int id {get;set;}
+    public string name{get;set;}
+    public string username{get;set;}
+    public string email{get;set;}
+    public address address{get;set;}
+    public string phone{get;set;}
+    public string website{get;set;}
+    public company company{get;set;}
+}
